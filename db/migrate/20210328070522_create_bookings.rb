44996d079db4cc819f7df690a14768c9dc9b9e67
Ryugi Suguro <rsuguro@gmail.com>
2021-03-28 16:16:20 +0900

    migrations fixed and added

--- db/migrate/20210328070522_create_bookings.rb
+++ db/migrate/20210328070522_create_bookings.rb
@@ -6,7 +6,7 @@ class CreateBookings < ActiveRecord::Migration[6.1]
       t.string :address
       t.string :status
       t.references :user, null: false, foreign_key: true
-      t.refernces :walker_id
+      t.references :walker, null: false, foreign_key: {to_table: :users}
 
       t.timestamps
     end
---
+++ db/migrate/20210328071349_add_last_nameto_user.rb
@@ -0,0 +1,4 @@
+class AddLastNametoUser < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
