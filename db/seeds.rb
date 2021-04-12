f7ad431dde4f1f4a18597539a1065142dd9be5db
Nick Roma <nikoandpiko@gmail.com>
2021-04-13 00:14:44 +0900

    Adds more to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "-- Cleaning DB"
+Booking.destroy_all
+Review.destroy_all
+User.delete_all
+
+puts "-- Creating Users"
+# Fake User for ease of logging in
+User.create(
+  email: "1@1.com",
+  password: "123456",
+  username: "Test",
+  address: "123 Fake Street",
+  bio: "Test bio"
+)
 
 User.create!(
   email: "nicholas@gmail.com",
+  password: "amadog1",
   username: "Nicholas",
-  address: "2-11-3 Meguro, Tokyo",
-  password: "amadog1"
+  address: "2-11-3 Meguro, Tokyo"
 )
 
 User.create!(
   email: "Christian@gmail.com",
+  password: "amadog2",
   username: "Christian",
-  address: "Haneda Airport, Ota City, Tokyo",
-  password: "amadog2"
+  address: "Haneda Airport, Ota City, Tokyo"
 )
 
 User.create!(
   email: "Edward@gmail.com",
+  password: "amadog3",
   username: "Edward",
-  address: "6-3-9 Akasaka, Minato City, Tokyo",
-  password: "amadog3"
+  address: "6-3-9 Akasaka, Minato City, Tokyo"
 )
+
+puts "-- Seeding Complete!"
