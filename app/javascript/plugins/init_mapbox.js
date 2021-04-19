5b75a546ff983e4eb1d6b5ea14f7c68ac82c4d6d
Lawrence Kroll <larry43.lk@gmail.com>
2021-04-19 21:07:47 +0900

    changed to previous map design

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -15,7 +15,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/satellite-v9' // here you can change style of the map
+      style: "mapbox://styles/mapbox/streets-v10" // here you can change style of the map
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
